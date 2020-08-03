986163c6de3c11968193f084a7f2cb45d84ab876
Nick <ptnguyen1901@gmail.com>
2020-08-03 06:14:00 -0400

    Initial Commit

---
+++ AppleWalletSwiftUI/AppleWalletSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWalletSwiftUIApp.swift
+//  AppleWalletSwiftUI
+//
+//  Created by Nick Nguyen on 8/3/20.
+//
+
+import SwiftUI
+
+@main
+struct AppleWalletSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
